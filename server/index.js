c12435e2ca80b5075cac859a2aa595ca106719be
Santhosh <pachasanthoshchowdary@gmail.com>
2022-02-11 17:29:40 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const CONNECTION_URL = "mongodb://localhost:27017/Travel-Blog";
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
+
 mongoose
   .connect(CONNECTION_URL, {
     useNewUrlParser: true,
@@ -19,7 +22,7 @@ mongoose
   })
   .then(() =>
     app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
+      console.log(`Server Running on Port: http://localhost:${PORT}/posts`)
     )
   )
   .catch((error) => console.log(`${error} did not connect`));
