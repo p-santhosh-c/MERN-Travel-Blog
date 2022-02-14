666dfd3189fcd461bb157f193f720711d30d427c
Santhosh <pachasanthoshchowdary@gmail.com>
2022-02-14 17:56:23 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import { getPosts } from './actions/posts';
-import Navbar from './components/Navbar/Navbar';
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Auth from "./components/Auth/Auth";
 
-const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
-  return (
+const App = () => (
+  <BrowserRouter>
     <Container maxWidth="lg">
       <Navbar />
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
     </Container>
-  );
-};
+  </BrowserRouter>
+);
 
 export default App;
