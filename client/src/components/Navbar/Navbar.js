f8b80ea746b92f847fa676b8b324ef7004b0682b
Santhosh <pachasanthoshchowdary@gmail.com>
2022-02-24 15:04:42 +0530

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Post</Typography>
       </div>
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
