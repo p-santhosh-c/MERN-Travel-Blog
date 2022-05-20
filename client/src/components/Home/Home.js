331ab06b6abb1e2d76e2d3c032a4aa75549f68f8
Santhosh <89576117+p-santhosh-c@users.noreply.github.com>
2022-05-20 19:52:43 +0530

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,30 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Container, Grow, Grid, AppBar, TextField, Button, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
 
-import { getPosts } from '../../actions/posts';
+import { getPostsBySearch } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination';
+import useStyles from './styles';
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 const Home = () => {
+  const classes = useStyles();
+  const query = useQuery();
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
+
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
+  const [search, setSearch] = useState('');
+  const history = useHistory();
+
+  const searchPost = () => {
+    if (search.trim()) {
+      dispatch(getPostsBySearch({ search}));
+      history.push(`/posts/search?searchQuery=${search || 'none'}`);
+    } else {
+      history.push('/');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      searchPost();
+    }
+  };
 
   return (
     <Grow in>
-      <Container>
-        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-          <Grid item xs={12} sm={7}>
+      <Container maxWidth="xl">
+        <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
+              <TextField onKeyDown={handleKeyPress} name="search" variant="outlined" style={{ margin: '10px 0' }} label="Search Posts" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
+              <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
           </Grid>
         </Grid>
+        {(!searchQuery) && (
+          <Paper className={classes.pagination} elevation={3}>
+            <Pagination page={page} />
+          </Paper>
+        )}
       </Container>
     </Grow>
   );
