b5ddfed39879d22c8e68e95593bab17093a66f08
Santhosh <89576117+p-santhosh-c@users.noreply.github.com>
2022-05-20 19:20:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = '';
+const CONNECTION_URL = 'MongoDB URL';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
