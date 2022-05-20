8b56a631a2aa6fef6b72bfd90ab1c8b0a3fcb006
Santhosh <89576117+p-santhosh-c@users.noreply.github.com>
2022-05-20 19:56:44 +0530

    Update styles.js

--- client/src/components/Posts/styles.js
+++ client/src/components/Posts/styles.js
@@ -1,56 +1,70 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
 
-export default makeStyles({
-  media: {
-    height: 0,
-    paddingTop: '56.25%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    backgroundBlendMode: 'darken',
-  },
-  border: {
-    border: 'solid',
-  },
-  fullHeightCard: {
-    height: '100%',
-  },
-  card: {
+export default makeStyles((theme) => ({
+  mainContainer: {
+    borderRadius: 15,
+    margin: '30px 0',
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    borderRadius: '15px',
-    height: '100%',
-    position: 'relative',
+    alignItems: 'center',
+    padding: '10px 50px',
   },
-  overlay: {
-    position: 'absolute',
-    top: '20px',
-    left: '20px',
-    color: 'white',
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+    textDecoration: 'none',
   },
-  overlay2: {
-    position: 'absolute',
-    top: '20px',
-    right: '20px',
-    color: 'white',
+  image: {
+    marginLeft: '15px',
   },
-  grid: {
+  toolbar: {
     display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
   },
-  details: {
+  profile: {
     display: 'flex',
     justifyContent: 'space-between',
-    margin: '20px',
+    width: '400px',
   },
-  title: {
-    padding: '0 16px',
-  },
-  cardActions: {
-    padding: '0 16px 8px 16px',
+  userName: {
     display: 'flex',
-    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  cardAction: {
-    display: 'block',
-    textAlign: 'initial',
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
   },
-});
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+  [theme.breakpoints.down('sm')]: {
+    appBar: {
+      padding: '10px 20px',
+    },
+    heading: {
+      display: 'none',
+    },
+    userName: {
+      display: 'none',
+    },
+    image: {
+      marginLeft: '5px',
+    },
+    toolbar: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      width: '160px',
+    },
+  },
+
+  actionDiv: {
+    textAlign: 'center',
+  },
+}));
+
