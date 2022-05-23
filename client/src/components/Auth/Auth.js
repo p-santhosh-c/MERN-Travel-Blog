1349a303e67711614ed8f7c540323f08129345d9
Santhosh <89576117+p-santhosh-c@users.noreply.github.com>
2022-05-23 10:38:42 +0530

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -46,7 +46,7 @@ const SignUp = () => {
     try {
       dispatch({ type: AUTH, data: { result, token } });
 
-      history.push('/');
+      history.push('/posts');
     } catch (error) {
       console.log(error);
     }
